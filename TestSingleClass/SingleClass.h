4e7e7ba51fa0dbf8c296cbb0c81061202998551d
Eddie_Fan <nmgfqlong@126.com>
2017-09-14 17:25:25 +0800

    add description

--- TestSingleClass/SingleClass.h
+++ TestSingleClass/SingleClass.h
@@ -9,7 +9,7 @@
 #import <Foundation/Foundation.h>
 
 @interface SingleClass : NSObject
-//设置一个只读属性，不可以改变。 这种属性用一个宏也可以代替吧。
+//设置一个只读属性，不可以改变。 这种属性全局用一个宏也可以代替吧。
 @property (nonatomic, strong, readonly) NSString *name;
 
 //设置属性，供外面全局使用
@@ -19,6 +19,7 @@
 //设置一个特定属性，直接访问这个属性，得到具体你设置的那个值，所以用重写get方法。
 //这完全可以用这个方法代替：- (BOOL)isLogin; 方法实现那里加上get方法的内容就是了。。
 @property (nonatomic, assign) BOOL isLogin;
+
 
 +(instancetype)shareSingleClass;
 
